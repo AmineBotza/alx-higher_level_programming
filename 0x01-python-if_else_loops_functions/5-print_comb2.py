539d88a6ec16b0e9cdd812c95af78e0976bb336c
abotza <botza.amine@gmail.com>
2024-01-07 09:30:45 +0100

    playing with digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for digits in range(100):
-    if digits == 99:
-        print(digits)
+for i in range(100):
+    if i == 99:
+        print(i)
     else:
-        print("{}".format('0' + str(digits) if digits < 10 else digits), end=", ")
+        print("{}".format('0' + str(i) if i < 10 else i), end=", ")

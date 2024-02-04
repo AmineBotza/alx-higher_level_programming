832f9e2cbc08c4729edfca0f6b57da439bf97155
abotza <botza.amine@gmail.com>
2024-02-04 13:35:01 +0100

    say my matrix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-"""A module for printing name."""
+""""
+
+This module contains a function that prints a name
+
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints full name(first_name last_name).
+    '''This function prints name (<first name> <last name>)
 
     Args:
-        first_name (str): first name
-        last_name (str): last name
-    raises:
-    """
-    # checks first_name and last_name type
+        first_name (str): The fisrt name to be printed
+        last_name (str): The last name to be printed
+
+    Raises:
+        TypeError: If either the first_name and last_name are not strings
+
+    '''
+
     if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
+        raise TypeError("first_name must be a string")
+
     if not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
-    # print name
-    print("My name is {} {}".format(first_name, last_name))
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
